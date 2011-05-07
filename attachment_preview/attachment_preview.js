1f5e66f94ec110eacdff998e75e223d19cc3b31f
thomasysliu <thomasysliu@gmail.com>
2011-05-08 01:34:13 +0800

    Add attachment download feature

--- attachment_preview/attachment_preview.js
+++ attachment_preview/attachment_preview.js
@@ -1,1 +1,22 @@
+/* Drag upload plugin script */
+if (window.rcmail) {
+
+	rcmail.addEventListener('init', function(evt) {
+			$(document).ready(function() {
+				if (rcmail.env.attachments)
+				{
+
+				var list = rcmail.gui_objects.attachmentlist.getElementsByTagName("li");
+				for (i=0; i<list.length; i++){
+
+				var url = rcmail.env.comm_path+'&_action=display-attachment&_file='+list[i].id+'&_id='+rcmail.env.compose_id;
+				list[i].innerHTML = list[i].innerHTML.replace(/\<\/a\>/, '</a><a href="'+url+'">');
+				begin=rcmail.gui_objects.attachmentlist.getElementsByTagName("a");
+				list[i].innerHTML += '</a>';
+
+				}
+
+				});
+			})
+	}
 
