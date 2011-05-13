3ea03d92050fe663107c5f7f17276467b28d0122
thomasysliu <thomasysliu@gmail.com>
2011-05-13 22:20:06 +0800

    Add Image preview, Thumbnailing

--- attachment_preview/attachment_preview.js
+++ attachment_preview/attachment_preview.js
@@ -1,4 +1,7 @@
 /* Drag upload plugin script */
+/* TODO: Preview, Hover enlarge, Hover Menu, Click Preview Window, Ajax Thumbnail loading
+
+ */
 if (window.rcmail) {
 
 	rcmail.addEventListener('init', function(evt) {
@@ -9,15 +12,31 @@ if (window.rcmail) {
 				var list = rcmail.gui_objects.attachmentlist.getElementsByTagName("li");
 				for (i=0; i<list.length; i++){
 
+				list[i].style.height="auto";
 				var url = rcmail.env.comm_path+'&_action=display-attachment&_file='+list[i].id+'&_id='+rcmail.env.compose_id;
+				var img_url = rcmail.env.comm_path+'&_action=plugin.preview&_file='+list[i].id+'&_id='+rcmail.env.compose_id;
 				list[i].innerHTML = list[i].innerHTML.replace(/\<\/a\>/, '</a><a href="'+url+'">');
 				begin=rcmail.gui_objects.attachmentlist.getElementsByTagName("a");
-				list[i].innerHTML += '</a>';
+				list[i].innerHTML += '</a><p><img src="'+img_url+'"></p>';
 
 				}
 
 				}
 				});
 			})
+	rcmail.ori_add2attachment_list=rcmail.add2attachment_list;
+	rcmail.add2attachment_list = function(name, att, upload_id) {
+		if(att.complete){
+			var url = rcmail.env.comm_path+'&_action=display-attachment&_file='+name+'&_id='+rcmail.env.compose_id;
+			var img_url = rcmail.env.comm_path+'&_action=plugin.preview&_file='+name+'&_id='+rcmail.env.compose_id;
+			att.html = att.html.replace(/\<\/a\>/, '</a><a href="'+url+'">');
+			att.html += '</a><p><img src="'+img_url+'"></p>';
+
+		}
+		this.ori_add2attachment_list(name, att, upload_id);
+		if(att.complete){
+			document.getElementById(name).style.height="auto";
+		}
+	};
 }
 
--- attachment_preview/attachment_preview.php
+++ attachment_preview/attachment_preview.php
@@ -23,21 +23,106 @@
  *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  *      MA 02110-1301, USA.
  */
-
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
 class attachment_preview extends rcube_plugin
 {
-  public $task = 'mail';
+	public $task = 'mail';
 
-  function init()
-  {
-    $rcmail = rcmail::get_instance();
-    if($rcmail->action == 'compose') {
-      $this->include_script('attachment_preview.js');
-      $this->add_texts('localization/', true);
-    }
+	function init()
+	{
+		$rcmail = rcmail::get_instance();
+		if($rcmail->action == 'compose') {
+			$this->include_script('attachment_preview.js');
+			$this->add_texts('localization/', true);
+		}
+		$this->register_action('plugin.preview', array($this, 'preview'));
 
-  }
+	}
+	function get_thumbnail($attachment,$ext)
+	{
+		switch(strtolower($ext)){
+			case "png":
+				$src = imagecreatefrompng($attachment);
+			break;
+			case "jpg":
+				case "jpeg":
+				//print "jpg";
+				$src = imagecreatefromjpeg($attachment);
+			break;
+			case "bmp":
+				$src = imagecreatefrombmp($attachment);
+			break;
+			case "gif":
+				$src = imagecreatefromgif($attachment);
+			break;
+			case "xpm":
+				$src = imagecreatefromxpm($attachment);
+			break;
+			case "xbm":
+				$src = imagecreatefromxbm($attachment);
+			break;
+			case "bmp":
+				$src = imagecreatefromwbmp($attachment);
+			break;
+			case "gd2":
+				$src = imagecreatefromgd2($attachment);
+			break;
+			case "gd":
+				$src = imagecreatefromgd($attachment);
+			break;
+			default:
+			header('Content-type: image/gif');
+			echo base64_decode('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+			break;
+		}
 
+		list($width,$height)=getimagesize($attachment);
+		$newwidth=120;
+		$newheight=($height/$width)*$newwidth;
+		$tmp=imagecreatetruecolor($newwidth,$newheight);
+
+		$newwidth1=120;
+		$newheight1=($height/$width)*$newwidth1;
+		$tmp1=imagecreatetruecolor($newwidth1,$newheight1);
+
+		imagecopyresampled($tmp,$src,0,0,0,0,$newwidth,$newheight,
+
+				$width,$height);
+
+		imagecopyresampled($tmp1,$src,0,0,0,0,$newwidth1,$newheight1, 
+
+				$width,$height);
+
+		header('Content-Type: image/jpeg');
+		// Skip the filename parameter using NULL, then set the quality to 75%
+		imagejpeg($tmp1, NULL, 75);
+
+	}
+	function preview()
+	{
+
+		$RCMAIL = rcmail::get_instance();
+		$COMPOSE_ID = get_input_value('_id', RCUBE_INPUT_GPC);
+		$_SESSION['compose'] = $_SESSION['compose_data'][$COMPOSE_ID];
+		if (preg_match('/^rcmfile(\w+)$/', $_GET['_file'], $regs))
+			$id = $regs[1];
+
+		if ($attachment = $_SESSION['compose']['attachments'][$id])
+			$attachment = $RCMAIL->plugins->exec_hook('attachment_display', $attachment);
+
+		if ($attachment['status']) {
+			if (empty($attachment['size']))
+				$attachment['size'] = $attachment['data'] ? strlen($attachment['data']) : @filesize($attachment['path']);
+
+			$path_info = pathinfo($attachment['name']);
+			$this->get_thumbnail($attachment['path'],$path_info['extension']);
+
+		}
+
+
+		exit;
+	}
 }
 
 ?>
